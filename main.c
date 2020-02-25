68d3e68384da746818f55b14d9733a9c6ad2c5a1
webturing <zj@webturing.com>
2020-02-25 11:04:01 +0800

    数组函数练习

--- main.c
+++ main.c
@@ -10,9 +10,8 @@ void print(int a[], int n) {
 }
 
 int main() {
-    int a[10]={1,2,3,4,5,6,7,8,9} ;
-
-    print(a, sizeof(a) / sizeof(a[0]));
-    print(a+1,5);//&a[i]=a+i
+    int a[10] ;
+    input(a,10);
+    print(a,10);
     return 0;
 }
